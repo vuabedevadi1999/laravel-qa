7b666b3ccc8621f4d740fa2c9a834b1eda918ec7
dopd <dopd@kaopiz.com>
2021-08-05 15:12:55 +0700

    delete question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -89,6 +89,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return redirect('/questions')->with('success','Xoa thanh cong');
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -35,6 +35,11 @@
                                     <div class="ml-auto">
                                         <a href="{{ route('questions.edit',$question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
                                     </div>
+                                    <form class="form-delete" action="{{ route('questions.destroy',$question->id) }}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure ?');">Delete</button>
+                                    </form>             
                                 </div>
                                 <p class="lead">
                                     Asked by
