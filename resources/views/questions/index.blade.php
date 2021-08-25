19224aadbbffebf665e56fecd1f5e9389278968b
dopd <dopd@kaopiz.com>
2021-08-25 13:34:42 +0700

    lesson-27

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -17,53 +17,13 @@
                 <div class="card-body">
                     @include('layouts._messages')
                     @forelse ($questions as $question)
-                        <div class="media">
-                            <div class="f-flex flex-column counters">
-                                <div class="vote">
-                                    <strong>{{ $question->votes_count }}</strong>{{ str_plural('vote',$question->votes_count) }}
-                                </div>
-                                <div class="status {{ $question->status }}">
-                                    <strong>{{ $question->answers_count }}</strong>{{ str_plural('answer',$question->answers_count) }}
-                                </div>
-                                <div class="view">
-                                    {{ $question->views ." " . str_plural('view',$question->views) }}
-                                </div>
-                            </div>
-                            <div class="media-body">
-                                <div class="d-flex align-items-center">
-                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>{{--hoặc $question->getUrlAttribute()--}}
-                                    <div class="ml-auto">
-                                        @can('update-question',$question){{--nếu dùng cách 2 đổi update-question bằng update(tên hàm để authorization việc cập nhật)--}}
-                                            <a href="{{ route('questions.edit',$question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endcan
-                                        @can('delete-question',$question){{--nếu dùng cách 2 đổi delete-question bằng delete(tên hàm để authorization việc xóa)--}}
-                                            <form class="form-delete" action="{{ route('questions.destroy',$question->id) }}" method="post">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure ?');">Delete</button>
-                                            </form>  
-                                        @endcan     
-                                    </div>    
-                                </div>
-                                <p class="lead">
-                                    Asked by
-                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                    <small class="text-muted">{{ $question->create_date }}</small>{{--hoặc $question->getCreateDateAttribute()--}}
-                                </p>
-                                <div class="excerpt">
-                                    {{ $question->excerpt(350) }}
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
+                        @include('questions._excerpt')
                     @empty
                         <div class="alert alert-warning">
                             <strong>Sorry</strong> These are no question avaiable
                         </div>
                     @endforelse
-                    <div class="mx-auto">
-                        {{ $questions->links() }}
-                    </div>
+                    {{ $questions->links() }}
                 </div>
             </div>
         </div>
